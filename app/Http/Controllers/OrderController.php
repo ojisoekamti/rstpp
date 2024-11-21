745c49fb4e301eff278cbb34bd86378efcb1f895
Ojisoekamti <abdulghoji31@gmail.com>
2024-11-21 09:48:05 +0700

    "Update confirmOrder method in OrderController to return false success status in JSON response"

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -63,7 +63,7 @@ class OrderController  extends Controller
             'totalAmount' => 'required|numeric',
         ]);
 
-        return response()->json(['success' => true, 'message' => 'Order saved successfully!', 'name' => $request->all()], 500);
+        return response()->json(['success' => false, 'message' => 'Order saved successfully!', 'name' => $request->all()], 500);
 
         // Start a database transaction to ensure both order and items are stored atomically
         DB::beginTransaction();
